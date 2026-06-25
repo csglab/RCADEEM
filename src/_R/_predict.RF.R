2dec13ed0c5da6fd2c55c809c9ce3162314c729c
Aldo Hernandez Corchado <aldohernandezcorchado@gmail.com>
2026-06-25 09:01:26 -0400

    Update _predict.RF.R

--- src/_R/_predict.RF.R
+++ src/_R/_predict.RF.R
@@ -1,23 +1,24 @@
-#  Copyright 2014 Hamed S. Najafabadi
-#  
-#  ********************************************************************
-#  
-#  This file is part of ZifRC package.
-#  
-#  ZifRC is free software: you can redistribute it and/or modify
+# =======================================================================
+#  Authors: Aldo Hernandez-Corchado, Hamed S. Najafabadi
+#
+#  Copyright 2026 Aldo Hernandez-Corchado, Hamed S. Najafabadi
+#
+#  This file is part of RCADEEM.
+#
+#  RCADEEM is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
-#  
-#  ZifRC is distributed in the hope that it will be useful,
+#
+#  RCADEEM is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
-#  along with ZifRC.  If not, see <http://www.gnu.org/licenses/>.
-#  
-#  ********************************************************************/
+#  along with RCADEEM.  If not, see <http://www.gnu.org/licenses/>.
+# =======================================================================
+
 # library(ggplot2)
 library(optparse)
 library(randomForest)
