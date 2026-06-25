d630ea7597e1769521f8341407628bb78f53e40e
Aldo Hernandez Corchado <aldohernandezcorchado@gmail.com>
2026-06-25 08:56:57 -0400

    Update postscript.cpp

--- src/RC/postscript.cpp
+++ src/RC/postscript.cpp
@@ -1,23 +1,23 @@
-// Copyright 2014 Hamed S. Najafabadi
-
-/********************************************************************
-
-This file is part of RCOpt.
-
-RCOpt is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-RCOpt is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with RCOpt.  If not, see <http://www.gnu.org/licenses/>.
-
-********************************************************************/
+//=======================================================================
+// Authors: Aldo Hernandez-Corchado, Hamed S. Najafabadi
+//
+// Copyright 2026 Aldo Hernandez-Corchado, Hamed S. Najafabadi
+//
+// This file is part of RCADEEM.
+//
+// RCADEEM is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RCADEEM is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with RCADEEM.  If not, see <http://www.gnu.org/licenses/>.
+//=======================================================================
 
 
 #include <iostream>
